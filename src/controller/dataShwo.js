98dbe5c66a905f4c9db814ef8f1a158fea8353b5
chingyang91@outlook.com <chingyang91@gmail.com>
2022-10-24 13:51:13 +0800

    Fix

--- src/controller/dataShwo.js
+++ src/controller/dataShwo.js
@@ -19,33 +19,38 @@ module.exports = class extends Base {
    * 数据统计页面查询
    */
   async dataShowAction() {
-    //查询订单详情信息
-    let data = await this.model(" orders a")
-      .field(
-        "COUNT(1) AS countSize , SUM(CASE WHEN a.order_type =1 THEN 1 ELSE 0 END) appletNum, " +
-          "SUM(CASE WHEN a.order_type =2 THEN 1 ELSE 0 END) systemNum," +
-          "SUM(CASE WHEN a.order_type =3 THEN 1 ELSE 0 END) welfareNum," +
-          "(SELECT CASE WHEN SUM(IFNULL(b.is_print,0)) IS NULL THEN 0 ELSE SUM(IFNULL(b.is_print,0)) END AS countNum  FROM order_timetable b   WHERE  a.id=b.orders_id AND a.status=1 AND b.is_print=1)AS  fill," +
-          "SUM(CASE WHEN a.pay_type =3 THEN 1 ELSE 0 END)  cancelNum"
-      ) //.where({"a.status":1})
-      .find();
-    //求总收入信息
-    let countMoney = await this.model("orders a ")
-      .field(
-        "CASE WHEN SUM(IFNULL(a.total_collection,0)) IS NULL THEN 0 ELSE SUM(IFNULL(a.total_collection,0)) END  num01," + //总收入
-          "CASE WHEN  SUM(IFNULL(a.order_rebates,0))IS NULL THEN 0 ELSE  SUM(IFNULL(a.order_rebates,0)) END  AS num02," + //总返点
-          "CASE WHEN  (SUM(IFNULL(a.total_collection,0))-SUM(IFNULL(a.order_rebates,0))) IS NULL THEN 0 ELSE  (SUM(IFNULL(a.total_collection,0))-SUM(IFNULL(a.order_rebates,0))) END  AS num03" //净收入
-      )
-      //净收入= 总收入-总返点
-      /**
-             * WHERE a.pay_type=1   1 已支付
-                AND a.is_return=1    1 已返点
-                AND a.status=0      1 未取消
-             */
-      .where({ "a.pay_type": 1, "a.status": 1 })
-      .find();
-    data.countMoney = countMoney;
-    return this.success(data);
+    const userId = this.post("userId");
+    if (userId === 1 || userId === 52) {
+      //查询订单详情信息
+      let data = await this.model(" orders a")
+        .field(
+          "COUNT(1) AS countSize , SUM(CASE WHEN a.order_type =1 THEN 1 ELSE 0 END) appletNum, " +
+            "SUM(CASE WHEN a.order_type =2 THEN 1 ELSE 0 END) systemNum," +
+            "SUM(CASE WHEN a.order_type =3 THEN 1 ELSE 0 END) welfareNum," +
+            "(SELECT CASE WHEN SUM(IFNULL(b.is_print,0)) IS NULL THEN 0 ELSE SUM(IFNULL(b.is_print,0)) END AS countNum  FROM order_timetable b   WHERE  a.id=b.orders_id AND a.status=1 AND b.is_print=1)AS  fill," +
+            "SUM(CASE WHEN a.pay_type =3 THEN 1 ELSE 0 END)  cancelNum"
+        ) //.where({"a.status":1})
+        .find();
+      //求总收入信息
+      let countMoney = await this.model("orders a ")
+        .field(
+          "CASE WHEN SUM(IFNULL(a.total_collection,0)) IS NULL THEN 0 ELSE SUM(IFNULL(a.total_collection,0)) END  num01," + //总收入
+            "CASE WHEN  SUM(IFNULL(a.order_rebates,0))IS NULL THEN 0 ELSE  SUM(IFNULL(a.order_rebates,0)) END  AS num02," + //总返点
+            "CASE WHEN  (SUM(IFNULL(a.total_collection,0))-SUM(IFNULL(a.order_rebates,0))) IS NULL THEN 0 ELSE  (SUM(IFNULL(a.total_collection,0))-SUM(IFNULL(a.order_rebates,0))) END  AS num03" //净收入
+        )
+        //净收入= 总收入-总返点
+        /**
+           * WHERE a.pay_type=1   1 已支付
+              AND a.is_return=1    1 已返点
+              AND a.status=0      1 未取消
+           */
+        .where({ "a.pay_type": 1, "a.status": 1 })
+        .find();
+      data.countMoney = countMoney;
+      return this.success(data);
+    }
+
+    return this.success({});
   }
 
   /**
@@ -74,166 +79,171 @@ module.exports = class extends Base {
    */
   async getOrdersByDayAction() {
     // 获取时间
-    let selectDate = moment(this.post("selectDate")).format("YYYY-MM-DD");
-    //  计算当天时间的收入信息
-    let dataDay = await this.model("v_sum_orders_amount")
-      .field(
-        "1 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint, 100 AS payment"
-      )
-      .where({ order_time: selectDate, suject: 1 })
-      .union(
-        "SELECT 2 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint,200 AS payment FROM v_sum_orders_amount WHERE order_time = '" +
-          selectDate +
-          "' AND suject = 2"
-      )
-      .select();
-    // 计算当天的实收 和应收
-    var dayAmount = [];
-    for (let index = 0; index < dataDay.length; index++) {
-      const element = dataDay[index];
-      if (element.type == 1) {
-        let suject1 = await this.model("orders")
-          .where({
-            order_time: selectDate,
-            suject: element.type,
-            pay_type: ["in", "1,3"],
-            is_return: 1,
-            STATUS: 1,
-            collection_type: 2,
-          })
-          .field("IFNULL(SUM(order_rebates),0) AS order_rebates")
-          .find();
-        dayAmount.push({
-          type: element.type,
-          printed: element.actule - element.noprint,
-          actule:
-            element.noprint * 80 +
-            (element.actule - element.noprint) * (80 + element.payment), // 科目二实际收款  打单数量 + 加上 未打单数量
-          receivable:
-            element.actule * 80 + element.actule * element.payment, // 科目二的应收款
-          order_rebates: suject1.order_rebates,
-        });
-      } else {
-        let suject2 = await this.model("orders")
-          .where({
-            order_time: selectDate,
-            suject: element.type,
-            pay_type: ["in", "1,3"],
-            is_return: 1,
-            STATUS: 1,
-            collection_type: 2,
-          })
-          .field("IFNULL(SUM(order_rebates),0) AS order_rebates")
-          .find();
-        dayAmount.push({
-          type: element.type,
-          printed: element.actule - element.noprint,
-          actule:
-            element.noprint * 80 +
-            (element.actule - element.noprint) * (80 + element.payment), // 科目三实际收款
-          receivable: element.actule * (80 + element.payment), // 科目三的应收款
-          order_rebates: suject2.order_rebates,
-        });
+    const userId = this.post("userId");
+    if (userId === 1 || userId === 52) {
+      let selectDate = moment(this.post("selectDate")).format("YYYY-MM-DD");
+      //  计算当天时间的收入信息
+      let dataDay = await this.model("v_sum_orders_amount")
+        .field(
+          "1 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint, 100 AS payment"
+        )
+        .where({ order_time: selectDate, suject: 1 })
+        .union(
+          "SELECT 2 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint,200 AS payment FROM v_sum_orders_amount WHERE order_time = '" +
+            selectDate +
+            "' AND suject = 2"
+        )
+        .select();
+      // 计算当天的实收 和应收
+      var dayAmount = [];
+      for (let index = 0; index < dataDay.length; index++) {
+        const element = dataDay[index];
+        if (element.type == 1) {
+          let suject1 = await this.model("orders")
+            .where({
+              order_time: selectDate,
+              suject: element.type,
+              pay_type: ["in", "1,3"],
+              is_return: 1,
+              STATUS: 1,
+              collection_type: 2,
+            })
+            .field("IFNULL(SUM(order_rebates),0) AS order_rebates")
+            .find();
+          dayAmount.push({
+            type: element.type,
+            printed: element.actule - element.noprint,
+            actule:
+              element.noprint * 80 +
+              (element.actule - element.noprint) * (80 + element.payment), // 科目二实际收款  打单数量 + 加上 未打单数量
+            receivable: element.actule * 80 + element.actule * element.payment, // 科目二的应收款
+            order_rebates: suject1.order_rebates,
+          });
+        } else {
+          let suject2 = await this.model("orders")
+            .where({
+              order_time: selectDate,
+              suject: element.type,
+              pay_type: ["in", "1,3"],
+              is_return: 1,
+              STATUS: 1,
+              collection_type: 2,
+            })
+            .field("IFNULL(SUM(order_rebates),0) AS order_rebates")
+            .find();
+          dayAmount.push({
+            type: element.type,
+            printed: element.actule - element.noprint,
+            actule:
+              element.noprint * 80 +
+              (element.actule - element.noprint) * (80 + element.payment), // 科目三实际收款
+            receivable: element.actule * (80 + element.payment), // 科目三的应收款
+            order_rebates: suject2.order_rebates,
+          });
+        }
       }
+      return this.success(dayAmount);
     }
-    return this.success(dayAmount);
+    return this.success({});
   }
   /**
    * 按月查询应收 实收款项
    */
   async getOrdersByMonthAction() {
     // 获取时间
-    let selectDate = moment(this.post("selectDate")).format("YYYY-MM-DD");
-    // 获取系统当前月份第一天
-    let firstDay = moment(selectDate).startOf("month").format("YYYY-MM-DD");
-    // 获取系统当前月份最后一天
-    let endDay = moment(selectDate).endOf("month").format("YYYY-MM-DD");
-    var monthAmount = [];
-    // 计算当月的实收和 应收
-    let dataMonth = await this.model("v_sum_orders_amount")
-      .field(
-        "1 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint, 100 AS payment"
-      )
-      .where({ order_time: { ">=": firstDay, "<=": endDay }, suject: 1 })
-      .union(
-        "SELECT 2 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint,200 AS payment FROM v_sum_orders_amount WHERE order_time >= '" +
-          firstDay +
-          "' and  order_time <= '" +
-          endDay +
-          "'  AND suject = 2 "
-      )
-      .select();
-    for (let index = 0; index < dataMonth.length; index++) {
-      const element = dataMonth[index];
-      if (element.type == 1) {
-        let suject1 = await this.model("orders")
-          .where({
-            order_time: { ">=": firstDay, "<=": endDay },
-            suject: element.type,
-            pay_type: ["in", "1,3"],
-            is_return: 1,
-            STATUS: 1,
-            collection_type: 2,
-          })
-          .field("IFNULL(SUM(order_rebates),0) AS order_rebates ")
-          .find();
-        // 查询  只付了定金没付尾款  的订单金额   :    统计逻辑为 :  预约了但是没有打单的数据
-        let payAmount = await this.model("v_pay_deposit")
-          .where({
-            order_time: { ">=": firstDay, "<=": endDay },
-            suject: element.type,
-          })
-          .field("IFNULL(SUM(v_pay_deposit.`collection`),0) AS deposit ")
-          .find();
+    const userId = this.post("userId");
+    if (userId === 1 || userId === 52) {
+      let selectDate = moment(this.post("selectDate")).format("YYYY-MM-DD");
+      // 获取系统当前月份第一天
+      let firstDay = moment(selectDate).startOf("month").format("YYYY-MM-DD");
+      // 获取系统当前月份最后一天
+      let endDay = moment(selectDate).endOf("month").format("YYYY-MM-DD");
+      var monthAmount = [];
+      // 计算当月的实收和 应收
+      let dataMonth = await this.model("v_sum_orders_amount")
+        .field(
+          "1 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint, 100 AS payment"
+        )
+        .where({ order_time: { ">=": firstDay, "<=": endDay }, suject: 1 })
+        .union(
+          "SELECT 2 AS type,IFNULL(count(1),0) AS actule,IFNULL(SUM(CASE WHEN print = 0 THEN 1 ELSE 0 END),0) AS noprint,200 AS payment FROM v_sum_orders_amount WHERE order_time >= '" +
+            firstDay +
+            "' and  order_time <= '" +
+            endDay +
+            "'  AND suject = 2 "
+        )
+        .select();
+      for (let index = 0; index < dataMonth.length; index++) {
+        const element = dataMonth[index];
+        if (element.type == 1) {
+          let suject1 = await this.model("orders")
+            .where({
+              order_time: { ">=": firstDay, "<=": endDay },
+              suject: element.type,
+              pay_type: ["in", "1,3"],
+              is_return: 1,
+              STATUS: 1,
+              collection_type: 2,
+            })
+            .field("IFNULL(SUM(order_rebates),0) AS order_rebates ")
+            .find();
+          // 查询  只付了定金没付尾款  的订单金额   :    统计逻辑为 :  预约了但是没有打单的数据
+          let payAmount = await this.model("v_pay_deposit")
+            .where({
+              order_time: { ">=": firstDay, "<=": endDay },
+              suject: element.type,
+            })
+            .field("IFNULL(SUM(v_pay_deposit.`collection`),0) AS deposit ")
+            .find();
 
-        monthAmount.push({
-          type: element.type,
-          printed: element.actule - element.noprint,
-          actule:
-            element.actule * 80 +
-            element.actule * element.payment +
-            element.noprint * 80, // 科目二实际收款
-          receivable:
-            element.actule * 80 + element.actule * element.payment, // 科目三的应收款
-          order_rebates: suject1.order_rebates,
-          deposit: payAmount.deposit, // 只付定金没有支付尾款的数据总和
-        });
-      } else {
-        let suject2 = await this.model("orders")
-          .where({
-            order_time: { ">=": firstDay, "<=": endDay },
-            suject: element.type,
-            pay_type: ["in", "1,3"],
-            is_return: 1,
-            STATUS: 1,
-            collection_type: 2,
-          })
-          .field("IFNULL(SUM(order_rebates),0) AS order_rebates ")
-          .find();
+          monthAmount.push({
+            type: element.type,
+            printed: element.actule - element.noprint,
+            actule:
+              element.actule * 80 +
+              element.actule * element.payment +
+              element.noprint * 80, // 科目二实际收款
+            receivable: element.actule * 80 + element.actule * element.payment, // 科目三的应收款
+            order_rebates: suject1.order_rebates,
+            deposit: payAmount.deposit, // 只付定金没有支付尾款的数据总和
+          });
+        } else {
+          let suject2 = await this.model("orders")
+            .where({
+              order_time: { ">=": firstDay, "<=": endDay },
+              suject: element.type,
+              pay_type: ["in", "1,3"],
+              is_return: 1,
+              STATUS: 1,
+              collection_type: 2,
+            })
+            .field("IFNULL(SUM(order_rebates),0) AS order_rebates ")
+            .find();
 
-        let payAmount = await this.model("v_pay_deposit")
-          .where({
-            order_time: { ">=": firstDay, "<=": endDay },
-            suject: element.type,
-          })
-          .field("IFNULL(SUM(v_pay_deposit.`collection`),0) AS deposit ")
-          .find();
+          let payAmount = await this.model("v_pay_deposit")
+            .where({
+              order_time: { ">=": firstDay, "<=": endDay },
+              suject: element.type,
+            })
+            .field("IFNULL(SUM(v_pay_deposit.`collection`),0) AS deposit ")
+            .find();
 
-        monthAmount.push({
-          type: element.type,
-          printed: element.actule - element.noprint,
-          actule:
-            element.actule * 80 +
-            element.actule * element.payment +
-            element.noprint * 80, // 科目三实际收款
-          receivable:
-            element.actule * 80 + element.actule * element.payment, // 科目三的应收款
-          order_rebates: suject2.order_rebates,
-          deposit: payAmount.deposit, // 只付定金没有支付尾款的数据总和
-        });
+          monthAmount.push({
+            type: element.type,
+            printed: element.actule - element.noprint,
+            actule:
+              element.actule * 80 +
+              element.actule * element.payment +
+              element.noprint * 80, // 科目三实际收款
+            receivable: element.actule * 80 + element.actule * element.payment, // 科目三的应收款
+            order_rebates: suject2.order_rebates,
+            deposit: payAmount.deposit, // 只付定金没有支付尾款的数据总和
+          });
+        }
       }
+      return this.success(monthAmount);
     }
-    return this.success(monthAmount);
+    return this.success({});
   }
 
   /**
